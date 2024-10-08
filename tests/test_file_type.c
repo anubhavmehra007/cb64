41d82cc0034717d91b9da5367e323366c382afc1
Anubhav Mehra <anubhav.mehra@hotmail.com>
2024-10-08 15:43:51 +0530

    Fixed test_file_type dependency on external files.

--- tests/test_file_type.c
+++ tests/test_file_type.c
@@ -4,9 +4,9 @@
 #include<string.h>
 
 Test(filesize, test_output) {
-	const char* test_file_path = "/home/anubhav/Downloads/sample-2.pdf";
+	const char* test_file_path = "./tests/test.txt";
 	FILE* file = fopen(test_file_path, "r");
 	char type[30];
 	file_type(file, type);
-	cr_expect(strcmp(type, "application/pdf") == 0); 
+	cr_expect(strcmp(type, "text/plain") == 0); 
 }
